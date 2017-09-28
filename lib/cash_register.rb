220db40386347c8285e16296c870d2bdb4a35079
alpbourne <alpbourne@gmail.com>
2017-09-28 02:56:07 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -19,7 +19,7 @@ class CashRegister
   end
 
   def apply_discount
-    @discounted_total = @total * (0.8)
+    @discounted_total = self.total * (0.8)
     @discounted_total = @discounted_total.to_i
     if @employee_discount > 0
       "After the discount, the total comes to #{@discounted_total}."
